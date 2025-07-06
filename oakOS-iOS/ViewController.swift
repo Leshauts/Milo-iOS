c59a4556b83640c77aef85ae67cf2770c962aa07
Léo Durand <leo.durand@gmx.com>
2025-07-06 09:54:25 +0200

    Initial Commit

---
+++ oakOS-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  oakOS-iOS
+//
+//  Created by Léo Durand on 06/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ oakOS-iOSTests/oakOS_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  oakOS_iOSTests.swift
+//  oakOS-iOSTests
+//
+//  Created by Léo Durand on 06/07/2025.
+//
+
+import Testing
+@testable import oakOS_iOS
+
+struct oakOS_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
