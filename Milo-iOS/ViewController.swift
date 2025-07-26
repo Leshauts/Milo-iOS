31e7ea58a3b6fd72e92eb5238ca21bd86ab26649
Léo Durand <leo.durand@gmx.com>
2025-07-26 16:27:04 +0200

    style: smooth launching animation

--- Milo-iOS/ViewController.swift
+++ Milo-iOS/ViewController.swift
@@ -2,36 +2,55 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
-    
+
     var webView: WKWebView!
-    
+
     override func loadView() {
+        // Créer une vue parent avec fond gris clair
+        let containerView = UIView()
+        containerView.backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1.0)
+        self.view = containerView
+
+        // Initialiser la WebView
         webView = WKWebView()
         webView.navigationDelegate = self
-        view = webView
+
+        // Fond WebView transparent pour laisser passer le fond de la vue
+        webView.backgroundColor = .clear
+        webView.isOpaque = false
+        webView.scrollView.backgroundColor = .clear
+
+        // Début invisible
+        webView.alpha = 0.0
+
+        // Ajouter la WebView dans la vue parente
+        containerView.addSubview(webView)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Garder le full screen
-        view.backgroundColor = UIColor.black
-        webView.backgroundColor = UIColor.black
+
         webView.scrollView.contentInsetAdjustmentBehavior = .never
-        
-        // User Agent personnalisé pour identifier l'app
-        webView.customUserAgent = "oakOS-iOS-App/1.0"
-        
-        let url = URL(string: "http://oakos.local")!
-        webView.load(URLRequest(url: url))
+        webView.customUserAgent = "Milo-iOS-App/1.0"
+
+        // Charger l'URL (après affichage pour éviter le lag)
+        DispatchQueue.main.async {
+            let url = URL(string: "http://milo.local")!
+            self.webView.load(URLRequest(url: url))
+        }
     }
-    
-    // Ajouter une classe CSS au body après chargement
+
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // Ajouter une classe CSS
         let script = "document.body.classList.add('ios-app');"
         webView.evaluateJavaScript(script, completionHandler: nil)
+
+        // Animation de fondu
+        UIView.animate(withDuration: 0.3) {
+            webView.alpha = 1.0
+        }
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         webView.frame = view.bounds
